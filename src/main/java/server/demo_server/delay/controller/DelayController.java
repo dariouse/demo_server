6d43941666d051ae41010bb823b89b87ee7b7136
dariouse <dariouse@gmail.com>
2025-08-05 12:39:36 +0900

    pkg 구조 수정

--- src/main/java/server/demo_server/delay/controller/DelayController.java
+++ src/main/java/server/demo_server/delay/controller/DelayController.java
@@ -1,4 +1,4 @@
-package server.delay.controller;
+package server.demo_server.delay.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
