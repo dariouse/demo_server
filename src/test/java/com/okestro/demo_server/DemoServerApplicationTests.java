6d13f2c2387f0660434bfeec857ed285dd1a93aa
dariouse <dariouse@gmail.com>
2025-08-05 10:05:32 +0900

    initial commit

---
+++ src/test/java/com/okestro/demo_server/DemoServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.okestro.demo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
